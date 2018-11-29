daeb43653bfdafbc0e96eedd9936bdccce07ed63
dsomel21@gmail.com <dsomel21@gmail.com>
2018-11-28 21:41:32 -0500

    running into siutations with graphql, forgot what the resolver does lol

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,5 +1,6 @@
-export const resolvers = {
+export const resolvers: IResolvers = {
   Query: {
-    hello: (_: any, { name }: any) => `Hello ${name || "World"}`
+    hello: (_: any, { name }: GQL.IHelloOnQueryArguments) =>
+      `Hello ${name || "World"}`
   }
 };
